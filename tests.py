6f4182f7d65e082e3b6e8a3a9d0ecef04e92a791
Andrey Antukh <niwi@niwi.be>
2013-09-07 11:55:17 +0200

    Add tests for huge file transfer.

--- tests.py
+++ tests.py
@@ -108,6 +108,29 @@ class PythonLibsshTest(unittest.TestCase):
         os.remove("/tmp/py-libssh.temp.file.2")
         os.remove("/tmp/py-libssh.temp.file.3")
 
+    def test_connect_and_get_huge_file(self):
+        sha1_1 = hashlib.sha1()
+
+        with io.open("/tmp/py-libssh.temp.file.2", "wb") as f:
+            data = b"K" * 10485760
+            sha1_1.update(data)
+            f.write(data)
+
+        session = self.pyssh.connect()
+        sftp = self.pyssh.Sftp(session)
+        sftp.get("/tmp/py-libssh.temp.file.2", "/tmp/py-libssh.temp.file.3")
+
+        self.assertTrue(os.path.exists("/tmp/py-libssh.temp.file.3"))
+
+        sha1_2 = hashlib.sha1()
+        with io.open("/tmp/py-libssh.temp.file.3", "rb") as f:
+            sha1_2.update(f.read())
+
+        self.assertEqual(sha1_2.hexdigest(), sha1_1.hexdigest())
+
+        os.remove("/tmp/py-libssh.temp.file.2")
+        os.remove("/tmp/py-libssh.temp.file.3")
+
 
     def test_read_remote_file(self):
         session = self.pyssh.connect()
