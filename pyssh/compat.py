4be5e9b86ad7bee3c198115269b8ae9bf9f1e442
Andrey Antukh <niwi@niwi.be>
2013-09-07 22:05:07 +0200

    Add to_text method to compat module with other minor fixes.

--- pyssh/compat.py
+++ pyssh/compat.py
@@ -2,12 +2,22 @@
 
 import six
 
-if six.PY2:
-    def to_bytes(data, encoding="utf-8"):
-        return data.encode(encoding)
-else:
-    def to_bytes(data, encoding="utf-8"):
-        return bytes(data, encoding)
-
 text_type = six.text_type
 binary_type = six.binary_type
+
+if six.PY2:
+    def to_bytes(data, encoding="utf-8"):
+        if isinstance(data, text_type):
+            return data.encode(encoding)
+        return data
+
+else:
+    def to_bytes(data, encoding="utf-8"):
+        if isinstance(data, text_type):
+            return bytes(data, encoding)
+        return data
+
+def to_text(data, encoding="utf-8"):
+    if isinstance(data, binary_type):
+        return data.decode(encoding)
+    return data
