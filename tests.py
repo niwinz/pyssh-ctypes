b7b6802f90cb302a30cfccd14dffcba637b25c7a
Andrey Antukh <niwi@niwi.be>
2013-09-08 13:05:25 +0200

    Adapt tests to last changes.

--- tests.py
+++ tests.py
@@ -30,7 +30,7 @@ class PythonLibsshTest(unittest.TestCase):
     def test_auth_with_password(self):
         # NOTE: this test asumes that your sistem has
         # test user with test password.
-        with self.pyssh.connect(username="test", password="test") as s:
+        with self.pyssh.new_session(username="test", password="test") as s:
             r = s.execute("id", lazy=False)
             result = r.as_bytes()
             self.assertEqual(result, b"uid=1001(test) gid=1001(test) groups=1001(test)\n")
@@ -40,10 +40,10 @@ class PythonLibsshTest(unittest.TestCase):
         # test user with test password.
 
         with self.assertRaises(self.pyssh_exp.AuthenticationError):
-            s = self.pyssh.connect(username="test", password="test2")
+            s = self.pyssh.new_session(username="test", password="test2", connect_on_init=True)
 
-    def test_connect_and_execute_command_not_lazy(self):
-        with self.pyssh.connect() as s:
+    def test_new_session_and_execute_command_not_lazy(self):
+        with self.pyssh.new_session() as s:
             r = s.execute("uname", lazy=False)
             result = r.as_bytes()
             return_code = r.return_code
@@ -52,8 +52,8 @@ class PythonLibsshTest(unittest.TestCase):
             self.assertEqual(result, b"Linux\n")
             self.assertIsInstance(r, self.pyssh_result.Result)
 
-    def test_connect_and_execute_command_01(self):
-        with self.pyssh.connect() as s:
+    def test_new_session_and_execute_command_01(self):
+        with self.pyssh.new_session() as s:
             r = s.execute("uname", lazy=True)
             result = r.as_bytes()
             return_code = r.return_code
@@ -62,28 +62,28 @@ class PythonLibsshTest(unittest.TestCase):
             self.assertEqual(result, b"Linux\n")
             self.assertIsInstance(r, self.pyssh_result.LazyResult)
 
-    def test_connect_and_execute_command_02(self):
-        with self.pyssh.connect() as s:
+    def test_new_session_and_execute_command_02(self):
+        with self.pyssh.new_session() as s:
             r = s.execute("uname", lazy=True)
             result = r.as_bytes()
 
             with self.assertRaises(RuntimeError):
                 result = r.as_bytes()
 
-    #def test_connect_and_execute_command_02(self):
-    #    s = self.pyssh.connect()
+    #def test_new_session_and_execute_command_02(self):
+    #    s = self.pyssh.new_session()
     #    r = s.execute("echo $FOO", env={"FOO": "Hello"})
     #    result = r.as_bytes()
     #    self.assertEqual(result, b"Hello")
 
-    def test_connect_and_put(self):
+    def test_new_session_and_put(self):
         sha1_1 = hashlib.sha1()
         with io.open("/tmp/py-libssh.temp.file.2", "wb") as f:
             data = b"FOOOO" * 20
             sha1_1.update(data)
             f.write(data)
 
-        with self.pyssh.connect() as s, s.create_sftp() as sftp:
+        with self.pyssh.new_session() as s, s.create_sftp() as sftp:
             sftp.put("/tmp/py-libssh.temp.file.2", "/tmp/py-libssh.temp.file.3")
 
         self.assertTrue(os.path.exists("/tmp/py-libssh.temp.file.3"))
@@ -97,14 +97,14 @@ class PythonLibsshTest(unittest.TestCase):
         os.remove("/tmp/py-libssh.temp.file.2")
         os.remove("/tmp/py-libssh.temp.file.3")
 
-    def test_connect_and_get(self):
+    def test_new_session_and_get(self):
         sha1_1 = hashlib.sha1()
         with io.open("/tmp/py-libssh.temp.file.2", "wb") as f:
             data = b"FOOOO" * 20
             sha1_1.update(data)
             f.write(data)
 
-        with self.pyssh.connect() as s, s.create_sftp() as sftp:
+        with self.pyssh.new_session() as s, s.create_sftp() as sftp:
             sftp.get("/tmp/py-libssh.temp.file.2", "/tmp/py-libssh.temp.file.3")
 
         self.assertTrue(os.path.exists("/tmp/py-libssh.temp.file.3"))
@@ -118,7 +118,7 @@ class PythonLibsshTest(unittest.TestCase):
         os.remove("/tmp/py-libssh.temp.file.2")
         os.remove("/tmp/py-libssh.temp.file.3")
 
-    def test_connect_and_get_huge_file(self):
+    def test_new_session_and_get_huge_file(self):
         sha1_1 = hashlib.sha1()
 
         with io.open("/tmp/py-libssh.temp.file.2", "wb") as f:
@@ -126,7 +126,7 @@ class PythonLibsshTest(unittest.TestCase):
             sha1_1.update(data)
             f.write(data)
 
-        with self.pyssh.connect() as s, s.create_sftp() as sftp:
+        with self.pyssh.new_session() as s, s.create_sftp() as sftp:
             sftp.get("/tmp/py-libssh.temp.file.2", "/tmp/py-libssh.temp.file.3")
 
         self.assertTrue(os.path.exists("/tmp/py-libssh.temp.file.3"))
@@ -142,14 +142,13 @@ class PythonLibsshTest(unittest.TestCase):
 
 
     def test_read_remote_file(self):
-        session = self.pyssh.connect()
-        sftp = self.pyssh.Sftp(session)
-        f = sftp.open("/tmp/py-libssh.temp.file", os.O_RDONLY)
-        self.assertEqual(b"aaaaaaaaa\n", f.read(10))
+        with self.pyssh.new_session() as s, s.create_sftp() as sftp:
+            f = sftp.open("/tmp/py-libssh.temp.file", os.O_RDONLY)
+            self.assertEqual(b"aaaaaaaaa\n", f.read(10))
 
     #def test_shell_01(self):
     #    import pdb; pdb.set_trace()
-    #    session = self.pyssh.connect()
+    #    session = self.pyssh.new_session()
     #    shell = session.shell()
     #    #x = shell.read(1024)
     #    #shell.write("echo $USER;\n")
